2237489d7f4baa0024ea3be405a80bc56cb92d42
akmal09 <akmalfauzan.af@gmail.com>
2025-05-05 22:02:59 +0700

    preparation for ci cd pipeline

--- product-service/src/test/java/com/project/ecommerce/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/project/ecommerce/ProductServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.project.ecommerce;
 
+import com.project.products.ProductServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = ProductServiceApplication.class)
 class ProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
