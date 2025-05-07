d9a818bf3949f06bc6a68461dcb5786f4ebf2cf0
akmal09 <akmalfauzan.af@gmail.com>
2025-05-07 07:03:25 +0700

    run 2

--- eureka-server/runner.bat
+++ eureka-server/runner.bat
@@ -1,4 +1,4 @@
-@REM Trigger deploy version 1.0
+@REM Trigger deploy version 1.1
 
 docker-compose --project-name "eureka-server" --down
 docker-compose --project-name "eureka-server" -f ./builder/docker-compose.yml up --build -d
