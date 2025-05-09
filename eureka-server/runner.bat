2e129a8578bf8668d4de5d09a932683038e64cba
akmal09 <akmalfauzan.af@gmail.com>
2025-05-10 06:53:49 +0700

    adjustment branch

--- eureka-server/runner.bat
+++ eureka-server/runner.bat
@@ -1,7 +1,7 @@
 @REM docker-compose --project-name "eureka-server" --down
 @REM docker-compose --project-name "eureka-server" -f ./builder/docker-compose.yml up --build -d
 echo "update latest repository"
-git pull origin development
+git pull origin publish
 timeout /t 3 /nobreak
 echo "build and test"
 mvn clean install
@@ -9,4 +9,4 @@ mvn clean install
 scp -i C:\Users\Akmal.Suranta\.ssh\id_ed25519 target/eureka-server-1.0.0.jar azureuser@20.247.180.27:/home/azureuser/list_jar
 
 timeout /t 5 /nobreak
-git push origin development
+git push origin publish
