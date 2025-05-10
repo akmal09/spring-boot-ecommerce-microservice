45c21a9dd6b3546c9156114b92f4e4fa27c5ff75
akmal09 <akmalfauzan.af@gmail.com>
2025-05-10 12:21:11 +0700

    add logging

--- eureka-server/runner.bat
+++ eureka-server/runner.bat
@@ -8,7 +8,7 @@ timeout /t 3 /nobreak
 call build.bat || exit /b 1
 
 echo add jar to server
-scp -i C:\Users\Akmal.Suranta\.ssh\id_ed25519 target/eureka-server-1.0.0.jar azureuser@20.247.180.27:/home/azureuser/list_jar
+scp -i C:\Users\Akmal.Suranta\.ssh\ecommerce-project-microservice target/eureka-server-1.0.0.jar azureuser@20.247.180.27:/home/azureuser/list_jar
 
 echo push code to repository
 timeout /t 5 /nobreak
