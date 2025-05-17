4e8c4037f90d153132bdd1caf985342895d4afcc
akmal09 <akmalfauzan.af@gmail.com>
2025-05-17 22:03:10 +0700

    operations service trigger deploy

--- operations-service/src/main/java/com/project/operations/OperationsServiceApplication.java
+++ operations-service/src/main/java/com/project/operations/OperationsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OperationsServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Operations service run");
 		SpringApplication.run(OperationsServiceApplication.class, args);
 	}
 
