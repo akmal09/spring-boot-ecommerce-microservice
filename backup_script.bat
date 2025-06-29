855164c4af633c2cd4556f88bab8f0f55224bd0c
akmal09 <akmalfauzan.af@gmail.com>
2025-06-29 16:27:40 +0700

    update

--- backup_script.bat
+++ backup_script.bat
@@ -1,8 +1,8 @@
 @echo off
 
-docker exec -t db-ecommerce-akmal pg_dump -U db-ecommerce-akmal -d db-ecommerce-akmal -f /tmp/db-ecommerce-akmal_backup.sql
+docker exec -t db-ecommerce-akmal pg_dump -U ecommerce-akmal -d ecommerce_akmal -f /tmp/db-ecommerce-akmal_backup.sql
 
-docker cp db-ecommerce-akmal:/tmp/alfitrah_backup.sql ./db-ecommerce-akmal_backup.sql
+docker cp db-ecommerce-akmal:/tmp/db-ecommerce-akmal_backup.sql ./db-ecommerce-akmal_backup.sql
 
 
 echo Done!
